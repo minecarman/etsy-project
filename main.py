61467335bde29a64b2e50df1a3c1a5a20a3acb71
Arda <minecarman@gmail.com>
2024-05-27 21:47:13 +0300

    cheapest

--- main.py
+++ main.py
@@ -5,4 +5,8 @@ etsydata = pd.read_csv('etsy-mens-t-shirts-11-15-2017.csv')
 def mostExpensive(data: pd.DataFrame):
     return data.nlargest(1, 'product_price')
 
-print(mostExpensive(etsydata)['url'])
+def mostCheapest(data: pd.DataFrame):
+    return data.nsmallest(1, 'product_price')
+
+
+print(mostCheapest(etsydata)['url'])
